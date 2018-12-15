1fb455498748313cf81de919068524e46ac3dffc
Artur Pawlak <pawlak.artur90@gmail.com>
2018-12-15 15:47:46 +0100

    Add RecipesList and Modal components to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import RecipesList from "./components/RecipesList";
+import extendedProvider from "./context/extendedProvider";
+import ManageRecipesContext from './context/ManageRecipesContext';
+import ManageRecipeModal from './components/ManageRecipeModal';
 
 class App extends Component {
+  static contextType = ManageRecipesContext;
+
+  openAddRecipeModal = () => {
+    this.context.toggleModal();
+  }
+
   render() {
+    const modal = this.context.showModal
+      ? <ManageRecipeModal />
+      : null;
     return (
       <div>
+        <RecipesList />
+        <button onClick={this.openAddRecipeModal}>Add Recipe</button>
+        {modal}
       </div>
     );
   }
 }
 
-export default App;
+export default extendedProvider(App);
